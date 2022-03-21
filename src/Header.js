e56364d3d58e3313db05404b0df87d676306b7ab
PERATHURU CHANDRAVAMSI <vamsiperathuru@gmail.com>
2022-03-21 16:46:27 +0530

    changes

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './Header.css';
-
+import i1 from "./images/i1.jpg";
 
 
 
@@ -18,7 +18,7 @@ function Header(){
     return(
      <div>
         <nav className="header">
-            <img className="header__logo"  alt="logo" src="https://www.linkpicture.com/q/logo1_7.jpg"/>
+            <img className="header__logo"  alt="logo" src={i1}/>
         
             
             
